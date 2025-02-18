8ef782b784426d8b62f39783aa6856cd5932d2ec
Seazerth <timmark2302@gmail.com>
2025-02-18 18:49:59 +0500

    Clear Project

---
+++ src/test/java/ru/morev/education/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.morev.education;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

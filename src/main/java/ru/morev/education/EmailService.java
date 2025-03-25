57a5ceb90df45ad834c24098308b5a1c790e34dc
Seazerth <timmark2302@gmail.com>
2025-03-25 23:07:54 +0500

    2 исправление

--- src/main/java/ru/morev/education/EmailService.java
+++ src/main/java/ru/morev/education/EmailService.java
@@ -3,11 +3,9 @@ package ru.morev.education;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
-@Primary // Делает этот бин приоритетным
 @Log4j2
 public class EmailService implements MessageService {
 
